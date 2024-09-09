b71998917f8fb05bcbb5539a7a8a8a3632441220
Khushi-Lb <khushi@lakebrains.in>
2024-09-09 17:58:00 +0530

    Learn PHP
    1 Variable.
    2 Aaray.
    3 Scoping global.
    4 Create a form.

---
+++ aaray.php
@@ -0,0 +1,17 @@
+<?php
+$fruits = array("Apple", "Banana", "Cherry");
+
+echo $fruits[0]; 
+echo $fruits[1]; 
+echo $fruits[2];
+
+$person = array(
+    "first_name" => "John",
+    "last_name" => "Doe",
+    "age" => 30
+);
+
+//access
+echo $person["first_name"]; 
+echo $person["age"]; 
+?>
